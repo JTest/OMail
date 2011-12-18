9e70a6c5d4b46febc178d46678c0d9d246ff9881
JerryZero <lwx169@gmail.com>
2011-12-18 12:53:03 +0800

    JerryZero_1 2011/12/18

--- src/omail/test/OMailTestActivity.java
+++ src/omail/test/OMailTestActivity.java
@@ -2,37 +2,55 @@ package omail.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class OMailTestActivity extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
-	private boolean acount_state=false;
+	public	SharedPreferences preferences;
+	public SharedPreferences.Editor editor;
+	//private boolean acount_state=false;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if(acount_state){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.main);
+       
+       //Button
+       View login_button = this.findViewById(R.id.login_button);
+       login_button.setOnClickListener(this);
+       
+       //preference
+       preferences=getSharedPreferences("account", MODE_WORLD_READABLE);
+	  	String address=preferences.getString("address", null);
+	  	String passwd=preferences.getString("passwd", null);
+    	if(null!=address||null!=passwd){
         	finish();
         	Intent i=new Intent(this, MainInterface.class);
         	startActivity(i);
-        }
-        setContentView(R.layout.main);
-        
-        //Button
-        View login_button = this.findViewById(R.id.login_button);
-        login_button.setOnClickListener(this);
+        }   	
     }
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		EditText addr_text=(EditText)findViewById(R.id.acount_entry);
+		String addr_str=addr_text.getText().toString();
+		EditText passwd_text=(EditText)findViewById(R.id.passwd_entry);
+		String passwd_str=passwd_text.getText().toString();
 		switch(v.getId())
 		{
 		case R.id.login_button:
+	    	preferences=getSharedPreferences("account", MODE_WORLD_READABLE);
+	    	editor=preferences.edit();
+			editor.putString("address", addr_str);
+	    	editor.putString("passwd", passwd_str);
+	    	editor.commit();
 			finish();
         	startActivity(new Intent(this, MainInterface.class));
 			break;
--- src/omail/test/SendMail.java
+++ src/omail/test/SendMail.java
@@ -7,6 +7,6 @@ public class SendMail extends Activity{
 	@Override
     public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
+		setContentView(R.layout.send_mail);
 	}
 }
